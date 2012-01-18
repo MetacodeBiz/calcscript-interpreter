97637df5d32b8787141225151be8d5e2b8609a4d
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2012-01-18 17:15:03 +0100

    Add sorting letters operation

--- src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/StringOperators.java
+++ src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/StringOperators.java
@@ -5,6 +5,8 @@ import biz.metacode.clients.calcscript.interpreter.ExecutionContext;
 import biz.metacode.clients.calcscript.interpreter.Invocable;
 import biz.metacode.clients.calcscript.interpreter.Value;
 
+import java.util.Arrays;
+
 public enum StringOperators implements Invocable {
     CONCATENATE {
 
@@ -25,6 +27,18 @@ public enum StringOperators implements Invocable {
             Value first = context.pop();
             try {
                 context.pushString(first.toString());
+            } finally {
+                first.release();
+            }
+        }
+    },
+    SORT_LETTERS {
+        public void invoke(ExecutionContext context) throws InterruptedException {
+            Value first = context.pop();
+            try {
+                char[] letters = first.toString().toCharArray();
+                Arrays.sort(letters);
+                context.pushString(new String(letters));
             } finally {
                 first.release();
             }

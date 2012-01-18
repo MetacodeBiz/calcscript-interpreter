db6a94f59dce94ded2fc0312a8c2dcc6a23a5058
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2012-01-18 18:53:01 +0100

    Add operation that executes block given number of times

--- src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/LoopOperators.java
+++ src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/LoopOperators.java
@@ -21,5 +21,14 @@ public enum LoopOperators implements Invocable {
                 value = context.pop();
             } while (value.toBoolean());
         }
-    }
+    },
+    TIMES {
+        public void invoke(ExecutionContext context) throws InterruptedException {
+            Invocable block = (Invocable) context.pop();
+            double times = context.popDouble();
+            for (int i = (int) times - 1; i >= 0; i--) {
+                block.invoke(context);
+            }
+        }
+    },
 }

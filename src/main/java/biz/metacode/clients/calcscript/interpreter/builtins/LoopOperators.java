c9b7e024288a7a53cc05954961360bbcc8d47299
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2012-01-18 11:55:28 +0100

    Add check for interrupted flag inside do loop

--- src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/LoopOperators.java
+++ src/main/java/biz/metacode/clients/calcscript/interpreter/builtins/LoopOperators.java
@@ -10,6 +10,9 @@ public enum LoopOperators implements Invocable {
             Invocable block = (Invocable) context.pop();
             double value = 0;
             do {
+                if (Thread.interrupted()) {
+                    throw new InterruptedException();
+                }
                 block.invoke(context);
                 value = context.popDouble();
             } while (value != 0);

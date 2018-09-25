b5fbe50a30814d6564b75d10feb516d264983e17
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2018-09-25 14:42:15 +0200

    Shorten method reference
    Lines exceeding 110 characters make checkstyle rules fail.

--- src/main/java/biz/metacode/calcscript/interpreter/source/package-info.java
+++ src/main/java/biz/metacode/calcscript/interpreter/source/package-info.java
@@ -1,9 +1,7 @@
 /**
  * Contains types that parse script's source.
  * {@link biz.metacode.calcscript.interpreter.source.Program} is an interpreter that when given
- * a script's source can execute it using
- * {@link
- * biz.metacode.calcscript.interpreter.source.Program#invoke(biz.metacode.calcscript.interpreter.ExecutionContext)}.
+ * a script's source can execute it using {@link biz.metacode.calcscript.interpreter.source.Program#invoke}.
  * <p>
  * This package also holds exceptions thrown when program has syntax errors.
  */
